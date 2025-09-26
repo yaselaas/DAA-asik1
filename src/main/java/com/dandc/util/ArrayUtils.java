6bae4a7a4e248b093a232b0d78eae44c0ff7de88
Assel <tynysbek.assel78@gmail.com>
2025-09-26 18:30:03 +0500

    refactor(util): partition, swap, shuffle, guards

--- src/main/java/com/dandc/util/ArrayUtils.java
+++ src/main/java/com/dandc/util/ArrayUtils.java
@@ -24,4 +24,42 @@ public class ArrayUtils {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+}package com.dandc.util;
+
+import java.util.Random;
+
+public class ArrayUtils {
+    private static final Random RANDOM = new Random();
+
+    // ... existing methods ...
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = RANDOM.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] generateRandomArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = RANDOM.nextInt(size * 10);
+        }
+        return arr;
+    }
+
+    public static void guardSmallArray(int[] arr, int minSize) {
+        if (arr.length < minSize) {
+            throw new IllegalArgumentException("Array too small, minimum size: " + minSize);
+        }
+    }
 }
